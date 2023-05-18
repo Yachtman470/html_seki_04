00bdb961ff0c6cc3a3ce5e11a184dbe41a160d2f
Yachtman470 <gooner1901@gmail.com>
2023-05-19 02:50:16 +0900

    Courseの雛形を作成

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-// jsを記述する際はここに記載していく
+// let testWidth = document.getElementById('para01').clientWidth;
+// let testHeight = document.getElementById('para01').clientHeight;
+// console.log(testWidth + 'px ' + testHeight + 'px');
+
+// $('#para02').height(testHeight);
